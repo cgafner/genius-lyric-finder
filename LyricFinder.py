bc975f509288caf49643bdef17e219bf6377ffd6
cgafner <74126898+cgafner@users.noreply.github.com>
2021-01-31 13:57:34 -0600

    Update LyricFinder.py
    Optimized for speed.

--- LyricFinder.py
+++ LyricFinder.py
@@ -13,20 +13,20 @@ import time
 
 
 def main():
-
     # Pass genius link to webdriver (browser)
     # Also sets window size to half screen width (left aligned)
 
     url = 'https://genius.com/'
     browser = webdriver.Chrome()
-    browser.get(url)
-    browser.set_window_size((GetSystemMetrics(0)/2), GetSystemMetrics(1))
+    browser.set_window_size((GetSystemMetrics(0) / 2), GetSystemMetrics(1))
     browser.set_window_position(-10, 0)
+    browser.get(url)
+
 
     # Finds the search bar element and enters user's song
 
-    browser.find_element(By.XPATH, '//input[@name="q"]').send_keys(input('Enter your song & artist name. (Ex. The Real Slim Shady Eminem): '))
-    time.sleep(2)
+    browser.find_element(By.XPATH, '//input[@name="q"]').send_keys(
+        input('Enter your song & artist name. (Ex. The Real Slim Shady Eminem): '))
 
     # Using ActionChains to 'press' Enter key.
 
@@ -36,19 +36,14 @@ def main():
 
     # Selects the first suggestion (If song is typed incorrectly, or isn't unique specifiying author may help avoid wrong selection error.)
 
-    time.sleep(2)
+    time.sleep(1)
     browser.find_element_by_class_name('mini_card').send_keys(Keys.ENTER)
 
-    # GETS TO SPECIFIED LYRICS. NOW COPY AND PRINT LYRICS
+    # Copies lyrics from first suggestion and prints to console.
 
-    time.sleep(2)
     lyrics = browser.find_element(By.CLASS_NAME, 'lyrics')
     print(lyrics.text)
-
-    # PRINTS LYRICS SUCCESSFULLY
-
-    time.sleep(1)
-
+    
     # Closes browser after printing lyrics
 
     browser.close()
@@ -68,5 +63,6 @@ def choice():
             main()
         if option[:1] == 'n':
             close()
+
 
 main()
